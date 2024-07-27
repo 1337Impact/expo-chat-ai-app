3c6c101e0846afe7d6754560327b20eb2a568b60
Mohammed Benkhattab <mobenkhattab@gmail.com>
2024-07-27 20:37:30 +0100

    feat: :sparkles: add nativewind

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Button, Text, TextInput, View } from "react-native";
+import { styled } from "nativewind";
+
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
+
+const StyledView = styled(View);
+const StyledText = styled(Text);
+const StyledButton = styled(Button);
+const StyledTextInput = styled(TextInput);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StyledView className="bg-red-100 h-full">
+      <StyledView className="flex-1 justify-center items-center">
+        <StyledText className="text-lg">
+          msgs
+        </StyledText>
+      </StyledView>
+      <StyledView className="flex flex-row justify-between border-2 rounded-md mx-4 mb-4">
+        <StyledTextInput className="w-[80%] p-1 px-2" placeholder="Message me"></StyledTextInput>
+        <StyledButton className="w-[20%] rounded-md bg-black" title="Send" onPress={() => {}}></StyledButton>
+      </StyledView>
+    </StyledView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
